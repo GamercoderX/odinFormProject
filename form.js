8254e1dbc3ef7a94abfab5a033d51ebae2558c10
GamercoderX <125307088+GamercoderX@users.noreply.github.com>
2023-07-16 19:43:58 +0100

    validations all done

--- form.js
+++ form.js
@@ -1,23 +1,24 @@
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.info = function(){
-        return`${title} by ${author}, ${pages} pages and ${read}.`
-    }
+const input = document.getElementById('check-pw');
+const original = document.getElementById('pw');
+const change = document.getElementById('pw-check-label');
+
+input.addEventListener('click', function(event) {
+  // Your code to be executed when the input is clicked
+  const clickedElement = event.target;
+  if(clickedElement!=input){
+    clickedElement.classList.add('blue');
   }
+  
+});
 
-  Book.prototype.sayHello = function() {
-    return "Hello, I'm a Book!"
- }
+input.addEventListener('input', function() {
+  // Your code to be executed when the input is clicked
 
-  const harryPotter1 = new Book("HP", "J.K.Rowling", "500", "have not read")
-  const harryPotter2 = new Book("HP", "J.K.Rowling", "1000", "have read")
-  console.log(harryPotter1.info())
-  console.log(harryPotter2.info())
 
-  console.log(Object.getPrototypeOf(harryPotter1) === Book.prototype)
-  console.log(Object.getPrototypeOf(harryPotter2) === Book.prototype)
-
-  console.log(harryPotter1.sayHello())
+  if(original.value!=input.value){
+    input.style.borderColor = 'red';
+  } else{
+    input.style.borderColor = 'lime';
+  }
+  
+});
